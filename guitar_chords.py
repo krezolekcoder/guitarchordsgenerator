c247805a0b03373cd5603420c58fc04e988f4e0b
krezolony <kkrezole@gmail.com>
2021-01-14 18:56:02 +0100

    Added major scale generation

--- guitar_chords.py
+++ guitar_chords.py
@@ -10,29 +10,52 @@ chromatic_scale_flats = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "
 chromatic_scale_sharps = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 
 # a recipie with steps for creating major scale 
+major_scale_steps = [2, 2, 1, 2, 2, 2, 1]
 
-
-def generate_notes_in_string(pitch_note ):
+def generate_chromatic_scale(root_note ):
     # TODO handle errors 
-    return  chromatic_scale_flats[chromatic_scale_flats.index(pitch_note) : ] + chromatic_scale_flats[: chromatic_scale_flats.index(pitch_note)]
+    return  chromatic_scale_flats[chromatic_scale_flats.index(root_note) : ] + chromatic_scale_flats[: chromatic_scale_flats.index(root_note)]
 
-def generate_scale()
 
+
+# TODO implement this 
+
+# Generating scale is based on the chromatic scale. User passes recipe which is the list of steps (1 -half step, 2-whole step) from notes starting from root_note 
+def generate_scale(root_note, recipe):
+    scale = [] 
+    index = 0
+    # generate chromatic scale - on top of this the major scale will be generated 
+    chromatic_scale_flats = generate_chromatic_scale(root_note)
+
+    # Add root note to the major scale list 
+    scale.append(chromatic_scale_flats[0])
+
+    # Iterate over chromatic scale list with given recipe 
+    for i in range(0, len(recipe) - 1): 
+        index = index + recipe[i] 
+        print(index)
+        scale.append(chromatic_scale_flats[index])
+
+    return scale 
+
+def generate_major_scale(root_note):
+    scale_steps = [2, 2, 1, 2, 2, 2, 1]
+    return generate_scale(root_note, scale_steps) 
+
+def generate_minor_scale(root_note):
+    pass
+
+# Function returns the degree note of major scale determined by root note 
+
+def get_major_scale_degree(root_note, degree_number):
+    pass
+
+def calculate_interval_between_notes(first_note, second_note):
+    pass
 if __name__ == "__main__":
 
-    # print(generate_notes_in_string('E'))
-    E_string = generate_notes_in_string('E')
-    B_string = generate_notes_in_string('B')
-    G_string = generate_notes_in_string('G')
-    D_string = generate_notes_in_string('D')
-    A_string = generate_notes_in_string('A')
-    E6_string = generate_notes_in_string('E')
 
-    print(E_string)
-    print(B_string) 
-    print(G_string)
-    print(D_string) 
-    print(A_string)
-    print(E6_string) 
+    C_major_scale = generate_scale('C', major_scale_steps)
+    print(C_major_scale)
 
 
